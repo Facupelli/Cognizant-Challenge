ded69afd6bafaa3873981903474ad3d88321bad1
Facupelli <facundopellicer4@gmail.com>
2022-02-14 16:46:07 -0300

    initial components

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import { Home } from './components/Home';
+import GlobalFonts from './fonts/fonts';
 
 function App() {
   return (
     <div >
-      <p>COGNIZANT CHALLENGE</p>
+      <GlobalFonts />
+      <Home />
     </div>
   );
 }
